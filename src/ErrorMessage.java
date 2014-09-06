1d4aced952f164ed7f315db3ed7dcbb11bccdfc8
ggt <ggt@ggt>
2014-09-06 20:17:57 +0800

    Added ErrorMessage class

---
+++ src/ErrorMessage.java
@@ -0,0 +1,17 @@
+
+public class ErrorMessage {
+
+    String msg;
+    DrawableBlock v;
+
+    public ErrorMessage() {
+	msg = "";
+	v = null;
+    }
+
+    public void setBlock(DrawableBlock block) { v = block; }
+    public DrawableBlock getBlock() { return v; }
+
+    public void setMsg(String m) { msg = m; }
+    public String getMsg() { return m; }
+};
